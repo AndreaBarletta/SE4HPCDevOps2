a186146f62eb47058c688cdbf7d1613c3a287825
yektaKamane <ykamaneh@gmail.com>
2024-06-03 08:08:53 +0000

    simple job schedule

---
+++ job1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=test_job      # Job name
+#SBATCH --output=output.txt      # Standard output and error log
+#SBATCH --error=error.txt        # Error log
+#SBATCH --time=00:01:00          # Wall time limit in HH:MM:SS
+#SBATCH --ntasks=1               # Number of tasks (processes)
+#SBATCH --cpus-per-task=1        # Number of CPU cores per task
+#SBATCH --mem=1GB                # Memory per node
+
+# Print a message
+echo "Hello, World!"
