ca41885c13a2534708da71af9553b11898c05cd0
yektaKamane <ykamaneh@gmail.com>
2024-06-03 07:53:36 +0000

    simple job schedule

--- job.sh
+++ job.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=matrix_multiplication
-#SBATCH --output=matrix_multiplication_output.txt
-#SBATCH --error=matrix_multiplication_error.txt
-#SBATCH --ntasks=2  # Adjust the number of tasks based on your MPI configuration
-#SBATCH --time=01:00:00  # Adjust the time limit as needed
-#SBATCH --partition=your_partition  # Replace with the appropriate partition for your job
 
+# Define the number of MPI processes
+NUM_PROCESSES=2
 
-# add jo
+# Define the output files for stdout and stderr
+STDOUT_FILE="output.txt"
+STDERR_FILE="error.txt"
+
+# Define the path to the Singularity Image File
+SIF_FILE="myimage.sif"
+
+# Run the application within the Singularity container using MPI
+singularity exec "$SIF_FILE" mpirun -np $NUM_PROCESSES main > "$STDOUT_FILE" 2> "$STDERR_FILE"
