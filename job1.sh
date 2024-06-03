b8cf8dc2eb4259231741a39230f461e0e022df4a
yektaKamane <ykamaneh@gmail.com>
2024-06-03 08:19:28 +0000

    dummy job

--- job1.sh
+++ job1.sh
@@ -3,9 +3,11 @@
 #SBATCH --output=output.txt      # Standard output and error log
 #SBATCH --error=error.txt        # Error log
 #SBATCH --time=00:01:00          # Wall time limit in HH:MM:SS
-#SBATCH --ntasks=1               # Number of tasks (processes)
+#SBATCH --ntasks=2               # Number of tasks (processes)
 #SBATCH --cpus-per-task=1        # Number of CPU cores per task
 #SBATCH --mem=1GB                # Memory per node
 
 # Print a message
-echo "Hello, World!"
+echo "Starting the script"
+
+build.sh
